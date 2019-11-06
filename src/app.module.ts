a8b1c6aa4d1bf9c23621a5937e5129ad2dffdead
princy <princy.raza@gmail.com>
2019-11-06 23:42:35 +0100

    Use env variables for DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraduationModule } from './graduation/graduation.module';
 import * as ormconfig from './ormconfig';
 
-export function DatabaseOrmModule(): DynamicModule {
-  // we could load the configuration from dotEnv here,
-  // but typeORM cli would not be able to find the configuration file.
-
-  return TypeOrmModule.forRoot(ormconfig);
-}
-
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig), GraduationModule],
 })
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,21 +1,39 @@
 import { ConnectionOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
-// You can load you .env file here synchronously using dotenv package (not installed here),
-// import * as dotenv from 'dotenv';
-// import * as fs from 'fs';
-// const environment = process.env.NODE_ENV || 'development';
-// const data: any = dotenv.parse(fs.readFileSync(`${environment}.env`));
+// load configs from the .env file if it exists
+// the .env file must be is at the root of the project
 // You can also make a singleton service that load and expose the .env file content.
-// ...
+const envFilePath: string = `.env`;
+let dbConf: any;
+if (fs.existsSync(envFilePath)) {
+  const env = dotenv.parse(fs.readFileSync(envFilePath));
+  dbConf = {
+    DB_HOST: env['DB_HOST'],
+    DB_PORT: env['DB_PORT'],
+    DB_USERNAME: env['DB_USERNAME'],
+    DB_PASSWORD: env['DB_PASSWORD'],
+    DB_NAME: env['DB_NAME'],
+  };
+} else {
+  dbConf = {
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+  };
+}
 
 // Check typeORM documentation for more information.
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'username',
-  password: 'password',
-  database: 'devops-test',
+  host: dbConf['DB_HOST'],
+  port: dbConf['DB_PORT'],
+  username: dbConf['DB_USERNAME'],
+  password: dbConf['DB_PASSWORD'],
+  database: dbConf['DB_NAME'],
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
 
   // We are using migrations, synchronize should be set to false.

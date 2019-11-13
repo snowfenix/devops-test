6ef0f4c774ec338b59f9828c2ed9c675562e3d6f
princy <princy.raza@gmail.com>
2019-11-13 23:36:46 +0100

    listening on  variable

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '0.0.0.0');
+  const port = process.env.PORT || 8080;
+  await app.listen(port, () => {
+    console.log('Hello world listening on port', port););
 }
 bootstrap();
